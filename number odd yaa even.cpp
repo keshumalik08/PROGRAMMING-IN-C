69ce68949b501ece041639cbdb8495ebd44acda5
Keshav Malik <keshumalik08@gmail.com>
2025-09-19 15:23:02 +0530

    Add files via upload

---
+++ number odd yaa even.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int n,r;
+printf("the number to be checked" );
+scanf("%d" , &n);
+r= n%2;
+if (r==0){
+	printf("the number is even  ");
+}
+else {
+	printf("the number is odd");
+}
+printf("KESHAV 125113047");
+return 0;
+}
