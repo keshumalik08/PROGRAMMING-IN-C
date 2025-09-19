2bbc64434a8752fc06ac50d17d778f829fd40693
Keshav Malik <keshumalik08@gmail.com>
2025-09-19 15:21:34 +0530

    Add files via upload

---
+++ simple interset.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	float p, r, t,SI;
+	
+	printf("Enter Priciple amount");
+	scanf("%f", &p);
+	
+	printf("Enter Rate");
+	scanf("%f", &r);
+	
+	printf("Enter time");
+	scanf("%f", &t);
+	
+	printf("SIMPLE INTERSET IS : %f",SI= p*r*t/100);
+	printf("KESHAV 125113047");
+	
+	return 0;
+}
